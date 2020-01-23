c4db2401f26669e8aff4daaa50055a18a2762036
technothon <pramachandran@sonusnet.com>
2020-01-23 09:50:37 +0000

    cdb directory handling changed

--- files/scripts/dumper.sh
+++ files/scripts/dumper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/sonus/sbx/tailf/bin/confd --cdb-debug-dump /opt/sonus/sbx/tailf/var/confd/cdb/ > /opt/sonus/sbx/tailf/var/confd/cdb/cdb-debug-dump.txt
-#> /opt/sonus/sbx/tailf/var/confd/cdb/cdb-debug-dump.txt
+cp -f /root/cdb/*.cdb /opt/sonus/sbx/tailf/var/confd/cdb/
+/opt/sonus/sbx/tailf/bin/confd --cdb-debug-dump /opt/sonus/sbx/tailf/var/confd/cdb/ > /root/cdb/cdb-debug-dump.txt
